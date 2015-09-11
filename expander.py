be6befcc99bbc6037c4fed784fc83a8a88b27ecf
nikolakn <nkcodeplus@gmail.com>
2015-09-11 20:48:47 +0000

    expander

--- expander.py
+++ expander.py
@@ -2,17 +2,23 @@
 import wiringpi2 as wiringpi
 from time import sleep
  
+
+pins = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+
 wiringpi.wiringPiSetup()
 wiringpi.mcp23s17Setup(65,0,0) # first pin,spi port,i2c address
-wiringpi.pinMode(72,1)     # sets pin of mcp23s17-0 to output
+for i in pins:
+    wiringpi.pinMode(i,1)     # sets pin of mcp23s17-0 to output
  
 try:
  while True:
-  wiringpi.digitalWrite(72,1) # sets  pin of mcp23s17-0 to 1 (3,3V, on)
-  sleep(2)
-  wiringpi.digitalWrite(72,0) # sets  pin of mcp23s17-0 to 0 (0V, off)
-  sleep(2)
+  for i in pins:   
+      wiringpi.digitalWrite(i,1) # sets  pin of mcp23s17-0 to 1 (3,3V, on)
+      sleep(1) 
+      wiringpi.digitalWrite(i,0) # sets  pin of mcp23s17-0 to 0 (0V, off)
+      sleep(1)
 except KeyboardInterrupt:
- wiringpi.digitalWrite(72,0)
- wiringpi.pinMode(72,0)
+ for i in pins:   
+     wiringpi.digitalWrite(i,0)
+     wiringpi.pinMode(i,0)
 
