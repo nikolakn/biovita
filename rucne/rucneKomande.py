b47db2ecb2a35ef346bc57566f942f8eb21f3190
nikolakn <nkcodeplus@gmail.com>
2016-01-28 02:36:17 +0100

    led

--- rucne/rucneKomande.py
+++ rucne/rucneKomande.py
@@ -20,14 +20,14 @@ class rucneProzor(QMainWindow,UiRucne.Ui_MainWindow):
         self.ctimer = QtCore.QTimer()
         QtCore.QObject.connect(self.ctimer, SIGNAL("timeout()"), self.ulaziUpdate)
         self.ctimer.start(200)
-        self.senzori =[self.donji_p1e,self.donji_p2e,self.donji_p3e,self.donji_p4e,
-                self.donji_p5e,self.donji_p6e,self.donji_p7e,self.donji_p8e,self.donji_p9e,
-                self.donji_p10e,self.donji_p11e,self.donji_p12e,self.donji_p13e,
-                self.donji_p14e,self.donji_p15e,self.donji_p16e,self.donji_p17e,
-                self.donji_p18e,self.donji_p19e,self.donji_p20e,self.donji_p21e,
-                self.donji_p22e,self.donji_p23e,self.donji_p24e,self.donji_p25e,
-                self.donji_p26e,self.donji_p27e,self.donji_p28e,self.donji_p29e,
-                self.donji_p30e,self.donji_p31e,self.donji_p32e]
+        self.senzori =[self.donji_p1e,self.donji_p3e,self.donji_p11e,self.donji_p7e,
+                self.donji_p8e,self.donji_p4e,self.donji_p2e,self.donji_p5e,self.donji_p6e,
+                self.donji_p14e,self.donji_p15e,self.donji_p16e,self.donji_p12e,
+                self.donji_p13e,self.donji_p25e,self.donji_s2e,self.donji_s1e,
+                self.donji_p17e,self.donji_p32e,self.donji_p27e,self.donji_p28e,
+                self.donji_p10e,self.donji_p20e,self.donji_p18e,self.donji_gotovmat,
+                self.donji_p19e,self.donji_p31e,self.donji_p30e,self.donji_puz24,
+                self.donji_p26e,self.donji_puz29]
     def initUI(self):
         self.setWindowIcon(QIcon('images/gear_blue.ico'))
         self.setWindowState(Qt.WindowMaximized)
@@ -35,12 +35,14 @@ class rucneProzor(QMainWindow,UiRucne.Ui_MainWindow):
         self.show()
             
     def ulaziUpdate(self):
-        self.state.updateSensors();
-        self.updateSen();
-
-    def updateSen(self):
-        self.senzori[0].on();
-        self.senzori[1].on();
-        self.senzori[2].on();
+        ulazi = self.state.updateSensors();
+        n = 0
+        for x in self.senzori:
+            if(ulazi[n]==1):
+                x.on();
+            else:
+                x.off();
+            n = n + 1;
         self.repaint() 
-        
+
+
