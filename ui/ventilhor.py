3e81b878eb0aa32d4103aced2f4733bf4de40864
nikolakn <nkcodeplus@gmail.com>
2016-03-02 21:24:20 +0100

    motori

---
+++ ui/ventilhor.py
@@ -0,0 +1,52 @@
+
+from PyQt4.QtGui import * # @UnusedWildImport
+from PyQt4.QtCore import * # @UnusedWildImport
+
+
+class VentilHor():
+    
+
+    def __init__(self,x,y,naziv=''):
+        self.crna = QColor(0, 0, 0)
+        self.crvena = QColor(255, 0, 0)
+        self.bela = QColor(255, 255, 255)
+        self.ukljuceno = False;
+        
+        #self.points = [QPoint(x, y), QPoint(x+30, y), QPoint(x, y+30), QPoint(x+30, y+30)]
+        self.points = [QPoint(x, y), QPoint(x, y+30), QPoint(x+30, y), QPoint(x+30, y+30)]
+        self.x = x;
+        self.y = y;
+        self.dx = self.x+15-25/2;
+        self.dy = self.y-28
+  
+        self.senzor = False
+    def on(self):
+        self.ukljuceno = True;  
+    def off(self):
+        self.ukljuceno = False; 
+    def senzorOn(self):
+        self.senzor = True;
+    def senzorOff(self):
+        self.senzor = False;
+        
+    def nacrtaj(self, paint):
+        paint.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(Qt.black, 1, Qt.SolidLine)
+        paint.setPen(pen)
+        needle =QPolygon(self.points)  
+        if (self.ukljuceno == True):
+            paint.setBrush(Qt.green)        
+            paint.drawPolygon(needle)
+        else: 
+            paint.setBrush(Qt.red) 
+            paint.drawPolygon(needle)             
+        if (self.senzor == True):
+            paint.setBrush(Qt.green) 
+            paint.drawEllipse(self.dx, self.dy, 25, 25)
+            paint.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+            paint.drawLine(self.dx+12, self.y-2,self.dx+12,self.y+15)
+        else:
+            paint.setBrush(Qt.red) 
+            paint.drawEllipse(self.dx, self.dy, 25, 25)  
+            paint.setPen(QPen(Qt.black, 2, Qt.SolidLine))            
+            paint.drawLine(self.dx+12, self.y-2,self.dx+12,self.y+15)            
--- ui/widgetNovi.py
+++ ui/widgetNovi.py
@@ -3,7 +3,7 @@ from PyQt4.QtGui import * # @UnusedWildImport
 from PyQt4.QtCore import * # @UnusedWildImport
 from motor import Motor
 from ventil import Ventil
-
+from ventilhor import VentilHor
 class noviWidget(QWidget):
     
     def __init__(self,parent=None):
@@ -17,13 +17,22 @@ class noviWidget(QWidget):
         #3: Motor(500,200,0), 4 : Motor(500,200,1),5: Motor(500,200,2), 6 : Motor(500,200,3),
         3: Motor(426,505,3,'traka2'),4: Motor(426,898,3,'redler_izn_silosa'),5: Motor(106,492,1,'traka1'),
         6: Motor(372,956,0,'red_u_jami'),7: Motor(656,996,3,'puz_jama'), 8: Motor(868,226,0,'iznad_binova'),
-        9: Motor(1364,311,2,'puz_89')}
+        9: Motor(1364,311,2,'puz_89'),
+        10: Motor(912,520,1,'bin1'),11: Motor(960,500,1,'bin6'),12: Motor(1045,520,1,'bin2'),
+        13 : Motor(1095,500,1,'bin5'),14: Motor(1173,520,1,'bin3'),15: Motor(1223,500,1,'bin4'),
+        16: Motor(1286,435,1,'bin9'),17: Motor(1323,435,1,'bin8'),18: Motor(1392,512,1,'bin7'),
+        19: Motor(966,756,0,'vagapuz'),20: Motor(962,913,0,'mlin'),21: Motor(1168,618,0,'mlin_elevator'),
+        22: Motor(1280,903,0,'mesaonapuz'),23: Motor(1458,377,0,'e3'),24: Motor(1191,753,2,'e_vaga'),
+        25: Motor(1500,792,2,'mesaona'),26: Motor(1590,367,3,'gotov_mat')}
         
         self.ventili = {1: Ventil(125,174,'sil6gore'),2: Ventil(262,174,'sil5gore'),3: Ventil(401,174,'sil4gore'),
                         4: Ventil(125,383,'sil6dole'),5: Ventil(262,383,'sil5dole'),6: Ventil(401,383,'sil4dole'),
                         7: Ventil(125,566,'sil1gore'),8: Ventil(262,566,'sil2gore'),9: Ventil(401,566,'sil3gore'),
                         10: Ventil(125,775,'sil1dole'),11: Ventil(262,775,'sil2dole'),12: Ventil(401,775,'sil3dole'),
-                        13: Ventil(907,282,'p1/6'),14: Ventil(1037,282,'p2/5'),15: Ventil(1170,282,'p2/4')}
+                        13: Ventil(907,282,'p1/6'),14: Ventil(1037,282,'p2/5'),15: Ventil(1170,282,'p2/4'),
+                        16: Ventil(1167,106,'pextruder'),
+                        17: VentilHor(1504,274,'got_ka_bin7'),18: VentilHor(1636,274,'pel_velika'),
+                        19: VentilHor(1534,196,'pel_mala')}
         
         
         self.motori[2].on()
@@ -36,7 +45,9 @@ class noviWidget(QWidget):
 
     def timerUpdate(self):
         self.repaint() 
-        
+    def mousePressEvent(self, event):
+        print 'x:'+str(event.x())+'y:'+str(event.y())
+      
     def paintEvent(self, QPaintEvent):
         paint = QPainter(self)
         paint.setPen(Qt.black);
